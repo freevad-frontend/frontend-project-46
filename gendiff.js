17eb3c408b89f552667c5b454f917a14c94ef333
Vadim Semenov <freevad@gmail.com>
2024-04-21 10:32:00 +0300

    step 2. add format to help

--- gendiff.js
+++ gendiff.js
@@ -5,6 +5,7 @@ program
 	.name('gendiff')
 	.description('Compares two configuration files and shows a difference.')
 	.version('1.0.0', '-V, --version', 'output the version number')
+	.option('-f, --format [type]', 'output format')
 	.option('-h, --help', 'output usage information')
 
 program.parse(process.argv);

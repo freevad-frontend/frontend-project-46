733ed13978a3bdc5b1888f5dc54107f8558e0555
Vadim Semenov <freevad@gmail.com>
2024-04-24 19:35:17 +0300

    Cognitive Complexity getDiffFiles

--- src/diff.js
+++ src/diff.js
@@ -3,14 +3,19 @@ import _ from 'lodash';
 import readFile from './reader.js';
 import parseFile from './parser.js';
 
+const getKeysUniqueSort = (file1, file2) => {
+  const keysAllUnique = _.union(Object.keys(file1), Object.keys(file2));
+  const keysSort = _.sortBy(keysAllUnique);
+  return keysSort;
+};
+
 const getDiffFiles = (filepath1, filepath2) => {
   const fileRead1 = readFile(filepath1);
   const fileRead2 = readFile(filepath2);
   const parseFile1 = parseFile(fileRead1);
   const parseFile2 = parseFile(fileRead2);
 
-  const keysAllUnique = _.union(Object.keys(parseFile1), Object.keys(parseFile2));
-  const keysSort = _.sortBy(keysAllUnique);
+  const keysSort = getKeysUniqueSort(parseFile1, parseFile2);
 
   const diffLines = keysSort.map((key) => {
     if (!_.has(parseFile2, key)) {
